79baaa913bfe3e28a3727e569ea3ac71700de482
JLCaballeroMQ <jcm1017@alu.ubu.es>
2024-01-27 17:22:20 -0500

    conexión wifi

---
+++ Hardware/boot.py
@@ -0,0 +1,3 @@
+from mi_wifi import WIFI
+from config import wifi_config
+wifi = WIFI(wifi_config["ssid"],wifi_config["password"]) #conecta a red wifi
---
+++ Hardware/config.py
@@ -0,0 +1,9 @@
+wifi_config = {
+    'ssid':'SSID',
+    'password':'PASSWORD'
+}
+
+utelegram_config = {
+    'token': 'TELEGRAM_TOKEN',
+    'chat_id': 'TU_ID'
+}
