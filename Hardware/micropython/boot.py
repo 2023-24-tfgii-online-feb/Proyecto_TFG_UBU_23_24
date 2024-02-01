42036be9744c335cf22d03d68a91fd25d0aea42a
JLCaballeroMQ <127446383+JLCaballeroMQ@users.noreply.github.com>
2024-02-01 02:44:00 +0100

    Delete Hardware directory
    Creada por error dedse VS Code.

--- Hardware/micropython/boot.py
+++
@@ -1,3 +0,0 @@
-from mi_wifi import WIFI
-from config import wifi_config
-wifi = WIFI(wifi_config["ssid"],wifi_config["password"]) #conecta a red wifi
--- Hardware/micropython/config.py
+++
@@ -1,9 +0,0 @@
-wifi_config = {
-    'ssid':'SSID',
-    'password':'PASSWORD'
-}
-
-utelegram_config = {
-    'token': 'TELEGRAM_TOKEN',
-    'chat_id': 'TU_ID'
-}
