40d409abb0466bbb7f2dab01c36025848191ce15
JLCaballeroMQ <jcm1017@alu.ubu.es>
2024-02-13 00:38:35 -0500

    Hardware: ordenando main.py

--- Hardware/micropython/main.py
+++ Hardware/micropython/main.py
@@ -23,43 +23,49 @@ led_humedad_suelo   = LedRGB(10,11,15)
 mqtt = MQTT(led_temperatura, led_humedad_ambiente, led_luminosidad, led_humedad_suelo)
 bot = TELEGRAM(led_temperatura, led_humedad_ambiente, led_luminosidad, led_humedad_suelo)
 #INTERVALO DE TIEMPO
-next_send, send_interval = 0, 0.5
+next_send, send_interval = 0, 60 #segundos
 #FUNCIONES
 def actualizar_next_send(tiempo_actual):
-    next_send = tiempo_actual + send_interval #en segundos
+    return tiempo_actual + send_interval #en segundos
 def manage_sending(temp, hum, lux, porcentaje_humedad_suelo):#gestiona envia alertas y datos
     bot.enviar_alertas_telegram(temp, hum, lux, porcentaje_humedad_suelo)
     mqtt.enviar("", "", temp, hum, lux, porcentaje_humedad_suelo)
-    actualizar_next_send(utime.time())
 def apagar_todo(led_temp,led_hum,led_lum,led_hs,oled):
     led_temp.apagar_todos()
     led_hum.apagar_todos()
     led_lum.apagar_todos()
     led_hs.apagar_todos()
     oled.apagar()
+def conteo_regresivo(siguiente, actual):
+    diferencia = siguiente - actual
+    if diferencia>=0:
+        print(f"Se enviará la data a DB y Telegram en: {diferencia} segundos")
 #BUCLE PRINCIPAL
 def main_loop():
     update_id = 0
     mqtt.setup_mqtt_subscription()
     ultimo_intento_reconexion = utime.time()
-    actualizar_next_send(utime.time())
+    next_send = actualizar_next_send(utime.time())
     while True:
         try:
             current_time = utime.time()
-            if current_time - ultimo_intento_reconexion > 120: # Reconectar MQTT si es necesario
+            # Reconectar MQTT si es necesario
+            if current_time - ultimo_intento_reconexion > 120:
                 mqtt.reconectar_mqtt()
                 ultimo_intento_reconexion = current_time
+            # Actualizar valores de sensores para mostrarlo en pantalla oled
             actualizar_valores_sensores(dht, humedad_suelo, luminosidad)
             oled.mostrar_datos_oled(dht.temperatura, dht.humedad, luminosidad.valor, humedad_suelo.valor)
-            # Procesar mensajes de Telegram si hay alguno
+            # Procesar mensajes de Telegram si hay alguno (ejm: comando /start)
             new_update_id, message = bot.read_message(update_id)
             if new_update_id != update_id and message:
                 update_id = new_update_id
                 bot.handle_telegram_commands(message['chat']['id'], message['text'],dht.temperatura,dht.humedad, luminosidad.valor, humedad_suelo.valor)
             # Enviar datos y alertas si es el momento
+            conteo_regresivo(next_send, current_time)
             if current_time >= next_send:
                 manage_sending(dht.temperatura,dht.humedad, luminosidad.valor, humedad_suelo.valor)
-                actualizar_next_send(current_time)
+                next_send = actualizar_next_send(current_time)
             #mqtt.client_sub.check_msg() # Verificar mensajes MQTT
             utime.sleep(0.1) # Pequeña pausa para evitar el uso excesivo de CPU
         except KeyboardInterrupt:
